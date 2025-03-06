2907e7cb8d5d43da40b48f5f132b63879846afda
KASHIF MAQBOOL JOIYA <km9924173@gmail.com>
2025-03-06 16:11:03 +0500

    New R file with a jupyter notebook has added

---
+++ R_Basics/1Basics.R
@@ -0,0 +1,44 @@
+# Print Hello world Program
+print("Hello World")
+
+
+# Variables
+x<-1L
+y<-2.2
+z <- x+y
+z
+
+# Type checking
+is.numeric(z)
+is.integer(x)
+
+a <- "Allah"
+is.character(a)
+
+b<- TRUE
+is.logical(b)
+
+# Class Checking
+class(a)
+class(b)
+class(x)
+class(y)
+class(z)
+
+
+# Type conversion
+class(x)
+print(as.character(x))
+print(as.numeric(x))
+print(as.logical(x))
+print(as.integer(y))
+
+
+# Order of Operators
+(10 +5)/3*5-4
+
+
+# Wording with Strings
+string <- "My name is Kashif Maqbool"
+string
+class(string)
